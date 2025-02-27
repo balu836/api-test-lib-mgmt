d7465442a85634601ab1d79caa11d8e54de0fcd0
bala.matam <bala.matam@engineering.digital.dwp.gov.uk>
2025-02-27 19:18:32 +0000

    added documentations for properties file

--- src/main/java/org/example/PropReader.java
+++ src/main/java/org/example/PropReader.java
@@ -5,6 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
+/**
+ * This is the class file to read the properties
+ */
 public class PropReader {
     static PropReader propReader;
     static Properties properties;
